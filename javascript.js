56e3b41301a942d207a2671f55099fe4853d7a6f
Asmacheb <chbasmaa@gmail.com>
2023-11-13 16:25:08 +0100

    js

---
+++ javascript.js
@@ -0,0 +1,52 @@
+// let nom = 0
+// let prénom = 0
+// let age = 2
+// let taille = 1.68
+
+// console.log ("prenom, nom, age, taille")
+// console.log ("prenom, nom, taille, age:" , age )
+
+//exo2
+
+// nom = "Chebaiki"
+// prenom = "Asma"
+// age = 24
+// taille = 1.68
+
+// let global = `${nom} ${prenom} ${age} ${taille} `
+
+// console.log (global)
+
+//EXO3
+
+// prenom = prompt("Entrer votre prenom");
+
+// console.log = (prenom);
+
+ //EXO4
+
+//  age = prompt ("entrer votre age")
+ 
+//  if (age>=18) {
+//     console.log("je suis majeur")
+//  }
+
+//  else {
+// console.log("je ne suis pas majeur")
+//  }
+
+
+//EXO5
+
+// let Mathéo = 15.4
+// let Nathan = 9.2
+// let Léo = 16.1
+// let Maxime = 12.6
+
+// let moyenne = (Mathéo+Nathan+Léo+Maxime) / 4; 
+// console.log (moyenne)
+
+//EXO6
+
+let joker = Math.floor(Math.random() * 100) + 1;
+console.log(joker)
